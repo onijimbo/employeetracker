62e49998c772e42355c1e5257e4d35e909c660c5
Riley Hall <onijimbo@gmail.com>
2020-04-23 18:52:34 -0700

    Added functions for view by department and view by manager.

--- index.js
+++ index.js
@@ -40,32 +40,15 @@ const manage = () =>{
     .then(function(respone){
         switch(respone.Manage){
             case 'View all employees':
-                console.log('You chose: View all employees')
+                // console.log('You chose: View all employees')
                 employeeView()
                 break;
             case 'View all employees by department':
-                inquire.prompt({
-                    type: 'list',
-                    message: 'Wich Department?',
-                    name: 'Department',
-                    choices: [
-                        'Zyuranger',
-                        'Mighty Morphin',
-                        'Kakuranger',
-                        'Alien Rangers'
-                    ]
-                })
-                .then(function(response1){
-                    switch(response1.Department){
-                        case 'Zyuranger':
-                            let targetId = 
-                            byDepartmentView()
-                    }
-                })
+                byDepartmentView()
 
                 break;
             case 'View all employees by manager':
-                console.log('You chose: View all employees by manager')
+                byManagerView()
                 break;
             case 'Add employee':
                 console.log('You chose: Add employee')
@@ -80,7 +63,7 @@ const manage = () =>{
                  console.log('You chose: Update employee\'s manager')
                 break;
             case 'Exit':
-                console.log('You chose: Exit')
+                connection.end();
                 break;        
                             
         }
@@ -88,36 +71,120 @@ const manage = () =>{
 };
 
 manage()
-
+// View all employees
 const employeeView = () =>{
-const qString = `SELECT employee.id, employee.first_name, employee.last_name, 
-roles.title, department.name as department, roles.salary, CONCAT(employee.first_name,'',employee.last_name) as manager 
-FROM employee LEFT JOIN roles ON (employee.role_id = roles.id) 
-LEFT JOIN department ON (roles.department_id = department.id)
-LEFT JOIN employee ON employee.id = employee.manager_id ORDER BY employee.id;`
+
+const qString = ` SELECT
+employee.id,
+employee.first_name,
+employee.last_name,
+department.name as department,
+roles.title,
+roles.salary,
+CONCAT(manager.first_name, ' ', manager.last_name) as manager
+FROM employee
+LEFT JOIN roles
+ON employee.role_id = roles.id
+LEFT JOIN department
+ON roles.department_id = department.id
+LEFT JOIN employee manager
+ON manager.id = employee.manager_id;`
 
 
 connection.query(qString, function(err, res){
+    if (err) throw err;
+    
     // console.log('hello')
     // console.log(res);
-    console.table(res)
+
+    console.table(res);
     manage()
 })
 
 
 };
 
-const byDepartmentView = (targetId) =>{
-
+const byDepartmentView = () =>{
+    connection.query('SELECT * FROM department', (err, name) =>{
+        // console.log(name[1].name);
+        if (err)
+        throw err;
+        inquire.prompt({
+            name: 'Department',
+            type: 'list',
+            message: 'Choose a department',
+            choices: function() {
+                let array = [];
+                for(let i = 0; i < name.length; i++){
+                    array.push({
+                        name: name[i].name,
+                        value: name[i].id
+                    });
+                }        
+                // console.log(choice);
+                return array;
+            }
+        })
+        .then(answer =>{
+            // console.log(answer);
+            const qString = `SELECT employee.first_name,
+            employee.last_name,
+            employee.id,
+            employee.role_id,
+            r.department_id,
+            r.title
+            FROM employee INNER JOIN roles r ON r.id = employee.role_id
+            INNER JOIN department ON r.department_id = department.id
+            where department.id = ?`;
+            connection.query(qString, [answer.Department], (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                manage()
+            })
+        });
+    })
    
-
-const qString = `SELECT employee.id, employee.first_name, employee.last_name,
-role.title, department.name, employee.manager_id
-FROM employee INNER JOIN department ON ()`
 };
 
 const byManagerView = () =>{
+    connection.query(`SELECT * FROM employee WHERE manager_id is null`, (err, res) =>{
+       
+        if (err)
+        throw err 
+        inquire.prompt({
+            name: 'Manager',
+            type: 'list',
+            message: 'choose a manager',
+            choices: function(){
+                let arr =  res.map(function(employee){
+                    return {name: employee.first_name +' '+ employee.last_name,
+                    value: employee.id};
+                });
 
+                
+
+                return arr;
+            }
+
+  
+        })
+        .then(answer =>{
+            const qString = `SELECT employee.first_name,
+            employee.last_name,
+            employee.id,
+            employee.role_id,
+            employee.manager_id,
+            r.title
+            FROM employee INNER JOIN roles r ON r.id = employee.role_id
+            WHERE manager_id = ?`
+            connection.query(qString, [answer.Manager], (err, res) =>{
+                if (err)
+                throw err
+                console.table(res);
+                manage()
+            });
+        })
+    })
 };
 
 const addEmployee = () =>{
@@ -136,6 +203,3 @@ const updateManger = () =>{
 
 };
 
-const exit = () =>{
-
-};
